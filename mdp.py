1918f832b166d43485dd03226c64015a5752de15
xanderhnasko <xanderh@stanford.edu>
2024-11-26 19:32:52 -0600

    playing with grouping regions

--- mdp.py
+++ mdp.py
@@ -1,30 +1,34 @@
 from packages import *
 from data import get_data   
 
-def analyze_data(): 
+def get_data(): 
     if not os.path.exists("data.pkl"):
         get_data()  
     df = pd.read_pickle("data.pkl") 
     column = df["wind_speed"]   
     print(column.describe())
-    print(df.head())
+    df["prefix"] = df["full_timeseries_path"].str.split("/", expand=True)[0]  
+    print(df)  
 
-    plt.hist(column, bins=20, color='c', edgecolor='black')   
-    plt.title(f'Histogram of Wind Speed, mu={column.mean():.2f}, sigma={column.std():.2f}')    
-    plt.xlabel('Wind Speed (m/s)')      
-    plt.ylabel('Frequency') 
-    plt.show()
-
-    print(df["site_id"].describe()) 
-    print(df["site_id"].nunique())  
-
+    df["lat_bin"] = df["latitude"]//1
+    df["long_bin"] = df["longitude"]//1
+    df["region_id"] = df["lat_bin"].astype(str) + "/" + df["long_bin"].astype(str)
+    grouped = df.groupby("region_id")
+    for region, group in grouped:
+        print(f"region: {region}")
+        print(group)
 
 
-    
-
+    # plt.hist(column, bins=20, color='c', edgecolor='black')   
+    # plt.title(f'Histogram of Wind Speed, mu={column.mean():.2f}, sigma={column.std():.2f}')    
+    # plt.xlabel('Wind Speed (m/s)')      
+    # plt.ylabel('Frequency') 
+    # plt.show()
+                
+  
 
 def main():
-    analyze_data()  
+    get_data()  
 
 if __name__ == "__main__":  
     main()
